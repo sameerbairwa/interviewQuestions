7b35254b895706298ee9beb11ef3e5d7e7176d57
sameerbairwa <sameerbairwa07@gmail.com>
2019-06-06 23:46:29 +0530

    Largest sequence of zeors

---
+++ binaryGap.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int n;
+    cin >> n;
+    int arr[32]; //  for 
+    while(n > 0){
+        arr[i] = n%2;
+        n=n/2;
+    }
+
+    return 0;
+}
