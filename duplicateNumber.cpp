238a4e2b8986c323f0fddf28b8fabef63b93ef70
sameerbairwa <sameerbairwa07@gmail.com>
2019-06-15 17:12:46 +0530

    add new questions

---
+++ duplicateNumber.cpp
@@ -0,0 +1,39 @@
+// arr - input array
+// size - size of array
+/*
+Given an array of integers of size n which contains numbers from 0 to n - 2. 
+Each number is present at least once. That is, 
+if n = 5, numbers from 0 to 3 is present in the given array at least once and one number is present twice.
+You need to find and return that duplicate number present in the array.
+Assume, duplicate number is always present in the array.
+Input format :
+Line 1 : Size of input array
+Line 2 : Array elements (separated by space)
+Output Format :
+Duplicate element
+Constraints :
+1 <= n <= 10^6
+Sample Input:
+9
+0 7 2 5 4 7 1 3 6
+Sample Output:
+7
+*/
+
+int MissingNumber(int arr[], int n){
+    /* Don't write main().
+     * Don't read input, it is passed as function argument.
+     * Return output and don't print it.
+     * Taking input and printing output is handled automatically.
+     */
+    int sum =0;
+    for(int i=0; i<n; i++){
+        sum += arr[i];
+    }
+    int s = (n-2)*(n-1)/2;
+    
+    return sum - s;
+} 
+
+
+
