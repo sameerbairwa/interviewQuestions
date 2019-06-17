39fd45ce3439d92e1bed692f878287c6eaade750
sameerbairwa <sameerbairwa07@gmail.com>
2019-06-18 00:52:24 +0530

    number pattern complete

--- numberPattern.cpp
+++ numberPattern.cpp
@@ -11,16 +11,32 @@ For eg. N = 5
 123    321
 1234  4321
 1234554321
+
+To solve this problem we break the problem into two parts 
+first is 
+1    
+12     
+123   
+1234  
+12345
+
+And second is 
+    1
+   21
+  321
+ 4321
+54321
+
 */
 
 int main() {
     int n; //  declare a variable for number of rows 
     cin >> n;
     for(int i=0; i<n; i++){
-        for(int j=1; j<=i+1; j++){
+        for(int j=1; j<=i+1; j++){ // for print first half
             cout << j;
         }
-        for(int k=0; k<n*2-2 - 2*i ; k++)
+        for(int k=0; k<n*2-2 - 2*i ; k++) // for generate space after first half printed 
         {
             cout << " ";
         }
