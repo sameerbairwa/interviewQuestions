be5fe968366a39834aafa05cc991e7443b75e4ea
sameerbairwa <sameerbairwa07@gmail.com>
2019-06-26 23:41:26 +0530

    new adding

---
+++ DynamicMemoryAllocation/memoryForString.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<malloc.h>
+
+int main(int argc, char const *argv[])
+{
+    char* p;
+    p = (char*)malloc(sizeof(char)*15); // when the expression executes 15 bytes of memory is allocated and p points to the first byte of allocated 15 byte block
+    gets(p); // for string input
+    printf("Hello %s\n"); 
+    return 0;
+}
+
+
