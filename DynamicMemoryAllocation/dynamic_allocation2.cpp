e0491889f609aceb46b1dbb325b5959db8e67515
sameerbairwa <sameerbairwa07@gmail.com>
2019-06-28 05:38:21 +0530

    dynamic allocation in C++

--- DynamicMemoryAllocation/dynamic_allocation2.cpp
+++ DynamicMemoryAllocation/dynamic_allocation2.cpp
@@ -3,14 +3,14 @@ using namespace std;
 
 int main() {
 
-	int *p = new int;
-	delete p;
+	int *p = new int; // dynamically allocated memory will not delete until we not delete it 
+	delete p; // delete the memory
 
 	p = new int;
 	delete p;
 
 	p = new int[100];
-	delete [] p;
+	delete [] p;  // array memory delete
 
 	/*
 	while (true) {
