bc5c0c8b23278dffc8de82d692273f00454fbeb4
sameerbairwa <sameerbairwa07@gmail.com>
2019-07-05 17:59:32 +0530

    sudoku Solver

---
+++ backtracking/sudoku.cpp
@@ -0,0 +1,84 @@
+#include <iostream>
+using namespace std;
+#define UNASSIGNED 0
+#define N 9
+bool FindUnassignedLocation(int grid[N][N],
+                            int &row, int &col);
+bool isSafe(int grid[N][N], int row,
+            int col, int num);
+
+bool sudokuSolver(int grid[N][N])
+{
+    int row, col;
+    if (!FindUnassignedLocation(grid, row, col))
+        return true; 
+    for (int num = 1; num <= 9; num++)
+    {
+        if (isSafe(grid, row, col, num))
+        {
+            grid[row][col] = num;
+            if(sudokuSolver(grid))
+                return true;
+            grid[row][col] = UNASSIGNED;
+        }
+    }
+    return false; 
+}
+
+/* Searches the grid to find an entry that is 
+still unassigned. If found, the reference 
+parameters row, col will be set the location 
+that is unassigned, and true is returned. 
+If no unassigned entries remain, false is returned. */
+bool FindUnassignedLocation(int grid[N][N],
+                            int &row, int &col)
+{
+    for (row = 0; row < N; row++)
+        for (col = 0; col < N; col++)
+            if (grid[row][col] == UNASSIGNED)
+                return true;
+    return false;
+}
+bool UsedInRow(int grid[N][N], int row, int num)
+{
+    for (int col = 0; col < N; col++)
+        if (grid[row][col] == num)
+            return true;
+    return false;
+}
+
+bool UsedInCol(int grid[N][N], int col, int num)
+{
+    for (int row = 0; row < N; row++)
+        if (grid[row][col] == num)
+            return true;
+    return false;
+}
+
+bool UsedInBox(int grid[N][N], int boxStartRow,
+               int boxStartCol, int num)
+{
+    for (int row = 0; row < 3; row++)
+        for (int col = 0; col < 3; col++)
+            if (grid[row + boxStartRow]
+                    [col + boxStartCol] == num)
+                return true;
+    return false;
+}
+
+bool isSafe(int grid[N][N], int row,
+            int col, int num)
+{
+
+    return !UsedInRow(grid, row, num) &&
+           !UsedInCol(grid, col, num) &&
+           !UsedInBox(grid, row - row % 3,
+                      col - col % 3, num) &&
+           grid[row][col] == UNASSIGNED;
+}
+
+int main(){
+
+
+    return true;
+}
