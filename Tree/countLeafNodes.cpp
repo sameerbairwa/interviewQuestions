0763f607c8c12dba03364df151ab170994097386
sameerbairwa <sameerbairwa07@gmail.com>
2019-08-10 00:50:07 +0530

    number of leaf nodes in generic tree

--- Tree/countLeafNodes.cpp
+++ Tree/countLeafNodes.cpp
@@ -82,19 +82,11 @@ int numLeafNodes(TreeNode<int> *root)
 
 int main()
 {
-    // TreeNode<int>* root = new TreeNode<int>(1);
-    // TreeNode<int> *node1 = new TreeNode<int>(2);
-    // TreeNode<int> *node2 = new TreeNode<int>(3);
-    // root->children.push_back(node1);
-    // root->children.push_back(node2);
 
-    //TreeNode<int>* root = takeInput();
     TreeNode<int> *root = takeInputLevelWise();
 
     //printLevelWise(root);
     int leafNodes = numLeafNodes(root);
 
-    // todo delete tree
-    //deleteTree(root);
     delete root;
 }
