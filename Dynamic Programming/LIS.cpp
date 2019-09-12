f38e569939d34411275e83bd69d25dc48ddfad67
sameerbairwa <sameerbairwa07@gmail.com>
2019-09-12 06:23:13 +0530

    Longest increasing subsequence

---
+++ Dynamic Programming/LIS.cpp
@@ -0,0 +1,56 @@
+#include <iostream>
+using namespace std;
+
+int lis(int *input, int n)
+{
+    int *output = new int[n];
+    output[0] = 1;
+    for (int i = 1; i < n; i++)
+    {
+        output[i] = 1;
+        for (int j = i - 1; j >= 0; j--)
+        {
+            if (input[j] >= input[i])
+            {
+                continue;
+            }
+            int possibleAns = output[j] + 1;
+            if (possibleAns > output[i])
+            {
+                output[i] = possibleAns;
+            }
+        }
+    }
+    int best = 0;
+    for (int i = 0; i < n; i++)
+    {
+        if (best < output[i])
+        {
+            best = output[i];
+        }
+    }
+    delete[] output;
+    return best;
+}
+
+int main()
+{
+    int n;
+    cin >> n;
+    int *input = new int[n];
+    for (int i = 0; i < n; i++)
+    {
+        cin >> input[i];
+    }
+    int ans = lis(input, n);
+    cout << ans << endl;
+    delete[] input;
+}
+/*
+6
+5 4 11 1 16 8
+Sample Output 1 :
+3
+Sample Output Explanation
+Length of longest subsequence is 3 i.e. (5,11,16) or (4,11,16).
+*/
