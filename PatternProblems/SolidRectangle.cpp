82e4294a1808212a3162f1b80fe92d5306c9f8d8
unknown <sameerbairwa07@gmail.com>
2020-07-07 04:17:20 -0700

    pattern problems

---
+++ PatternProblems/SolidRectangle.cpp
@@ -0,0 +1,35 @@
+/*
+Problem: print solid rectangle using star
+Input format: n m 
+n: number of rows 
+m: number of columns 
+
+Output format: solid rectangle 
+Example: 3 5
+
+* * * * *
+* * * * *
+* * * * * 
+
+*/
+
+#include <iostream>
+using namespace std;
+
+void printRectangle(int n, int m){
+    for(int i=0; i<n; i++){
+        for(int j=0; j<m; j++){
+            cout << "*" << " ";
+        }
+        cout << "\n";  // cout << endl;
+    }
+}
+
+int main(){
+    int n,m;
+    // n => number of rows 
+    // m => number of columns 
+    cin >> n >> m;
+    printRectangle(n,m);
+
+}
